7f042407d2ab80ee4bd05ea498ee4d6dc50cb186
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-06-21 21:42:24 +0530

    adjust the navbar

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -6,10 +6,19 @@ export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
   const navigate = useNavigate();
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(true);
   const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setshowUserLogin] = useState(false);
 
-  const value = { navigate, user, setUser, isSeller, setIsSeller };
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setshowUserLogin,
+  };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
