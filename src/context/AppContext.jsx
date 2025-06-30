e70ce9af40d5526af653d5b9fc1a549d215383ff
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-06-30 13:39:07 +0530

    complete the product alightment

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -16,6 +16,8 @@ export const AppContextProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
 
   const [cartItems, setCartItems] = useState({});
+  const [searchQuery, setSearchQuery] = useState({});
+
   // fetch all products
   const fetchProducts = async () => {
     setProducts(dummyProducts);
@@ -72,6 +74,8 @@ export const AppContextProvider = ({ children }) => {
     updateCartItem,
     removeFromCart,
     cartItems,
+    searchQuery,
+    setSearchQuery,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
