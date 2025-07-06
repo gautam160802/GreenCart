984da731fa1d6f0833cc7c988b47c82d067c7f72
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-06 18:50:05 +0530

    adding frontend to backend

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,6 +3,10 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyProducts } from "../assets/assets";
 import toast from "react-hot-toast";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
 export const AppContext = createContext();
 
@@ -97,6 +101,7 @@ export const AppContextProvider = ({ children }) => {
     setSearchQuery,
     getCartAmount,
     getCartCount,
+    axios,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
