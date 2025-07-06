f5297b564716e57ad07b394de9bfc7a24a59cd18
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-06 17:21:03 +0530

    completed api address

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -15,15 +15,45 @@ export const addProduct = async (req, res) => {
         return result.secure_url;
       })
     );
-    await Product;
-  } catch (error) {}
+    await Product.create({ ...productData, image: imageUrl });
+    res.json({ success: true, message: "Product Added" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 // get product : /api/product/ist
-export const productList = async (req, res) => {};
+export const productList = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json({ success: true, products });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // get single product : /api/product/id
-export const productById = async (req, res) => {};
+export const productById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const product = await Product.findById(id);
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // change product inStock : /api/product/stock
-export const changeStock = async (req, res) => {};
+export const changeStock = async (req, res) => {
+  try {
+    const { id, inStock } = req.body;
+    await Product.findByIdAndUpdate(id, { inStock });
+    res.json({ success: true, message: "Stock Updated" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
